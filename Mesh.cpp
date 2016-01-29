c7a4e0992f8f750fd2bb00a9d4e5f9a27e5483d0
owenchj <ownechj@gmail.com>
2016-01-29 12:08:35 +0100

    Find triangles' neighbours -- finish

--- Mesh.cpp
+++ Mesh.cpp
@@ -16,6 +16,7 @@
 // for more details.                                                          
 // --------------------------------------------------------------------------
 
+
 #include "Mesh.h"
 #include <iostream>
 #include <fstream>
@@ -43,6 +44,16 @@ void Mesh::loadOFF (const std::string & filename) {
     in.close ();
     centerAndScaleToUnit ();
     recomputeNormals ();
+
+    // Test function
+    findNeighbours();
+    shapeApproximation();
+    
+    // Vec3f a(0.0,3.0,0.0);
+    // Vec3f b(4.0,0.0,0.0);
+    // Vec3f c(0.0,0.0,0.0);
+    // Proxy p(a,b);
+    // distortionError(p,a,b,c);
 }
 
 void Mesh::recomputeNormals () {
@@ -73,4 +84,114 @@ void Mesh::centerAndScaleToUnit () {
     }
     for  (unsigned int i = 0; i < V.size (); i++)
         V[i].p = (V[i].p - c) / maxD;
+}
+
+
+
+
+
+/// E = |ni - Ni|^2 * |Ti|; |Ti| = area of triangle T
+float Mesh::distortionError(Proxy p, Vec3f t0, Vec3f t1, Vec3f t2) {
+  Vec3f n;
+  float err;
+  float distance2, area;
+
+  Vec3f e0 = t1 - t0;
+  Vec3f e1 = t2 - t0;
+  
+  n = cross(e0, e1);
+  area = 0.5 * n.length();
+
+  n.normalize();
+  distance2 = pow(dist(n, p.n), 2);
+    
+  err = distance2 * area;
+
+  // cout <<n << endl;  
+  // cout <<area<< endl;  
+  // cout <<err<< endl;  
+  
+  return err;
+}
+
+
+
+bool Mesh::isNeighbour(Triangle &T0, Triangle &T1) {
+  Vec3f t0[3];
+  Vec3f t1[3];
+  int cors = 0;
+
+  for (unsigned int i = 0; i < 3; i++)
+    {
+      t0[i]= V[T0.v[i]].p ;
+      t1[i]= V[T1.v[i]].p ;
+    }
+
+  for (unsigned int i = 0; i < 3; i++)
+    {
+      if(t0[i] == t1[0] || t0[i] == t1[1] || t0[i] == t1[2])
+	cors++;
+    }
+  
+  if(cors == 2)
+    return true;
+  else 
+    return false;
+}
+
+void Mesh::shapeApproximation() {
+
+  std::vector<Triangle > errQue(2 * T.size());
+  
+  int t[num] ;
+  Proxy p[num];
+  
+  /* initialize random seed: */
+  srand (time(NULL));
+  
+  /* generate secret number between 1 and 10: */
+  for (unsigned int i = 0; i < num; i++){
+    t[i]= rand() % T.size() + 1;
+    cout << t[i] << endl;
+  }
+  
+  for (unsigned int i = 0; i < num; i++)
+    {
+
+    }
+  
+  
+}
+
+
+void Mesh::findNeighbours() {
+
+  int cnt = 0;
+  Neighbours nb;
+  
+  for (unsigned int i = 0; i < T.size (); i++)
+    {
+      nb.t[0] = -1;
+      nb.t[1] = -1;
+      nb.t[2] = -1;
+      cnt = 0;
+      for (unsigned int j = 0; j < T.size (); j++){
+	if(j != i){
+	  if(isNeighbour(T[i], T[j]) == true){
+	    nb.t[cnt++] = j;
+	  }
+	}
+      }
+
+      T[i].neighbour = nb;      
+    }
+
+   // for (unsigned int i = 0; i < T.size (); i++)
+   //   {
+   //     cout << T[i].neighbour.t[0] << " " ; 
+   //     cout << T[i].neighbour.t[1] << " "; 
+   //     cout << T[i].neighbour.t[2] << endl; 
+   //   } 
+
+   //recomputeNormals ();
 }
--- Mesh.h
+++ Mesh.h
@@ -20,55 +20,104 @@
 #include <cmath>
 #include <vector>
 #include "Vec3.h"
+#include <time.h>       /* time */
+
+#define num 6
+
+
+///
+class Neighbours {
+ public:
+  inline Neighbours () { t[0] = 0; t[1] = 0; t[2] =  0; }
+  inline Neighbours (const int & t0, const int & t1, const int & t2) {
+    t[0] = t0; t[1] = t1; t[2] =  t2;
+  }
+  inline virtual ~Neighbours () {}
+  int t[3];
+};
 
 /// A simple vertex class storing position and normal
 class Vertex {
-public:
-    inline Vertex () {}
-    inline Vertex (const Vec3f & p, const Vec3f & n) : p (p), n (n) {}
-    inline virtual ~Vertex () {}
-    Vec3f p;
-    Vec3f n;
+ public:
+  inline Vertex () {}
+  inline Vertex (const Vec3f & p, const Vec3f & n) : p (p), n (n) {}
+  inline virtual ~Vertex () {}
+  Vec3f p;
+  Vec3f n;
 };
 
 /// A Triangle class expressed as a triplet of indices (over an external vertex list)
 class Triangle {
-public:
-    inline Triangle () {
-        v[0] = v[1] = v[2] = 0;
-    }
-    inline Triangle (const Triangle & t) {
-        v[0] = t.v[0];
-        v[1] = t.v[1];
-        v[2] = t.v[2];
-    }
-    inline Triangle (unsigned int v0, unsigned int v1, unsigned int v2) {
-        v[0] = v0;
-        v[1] = v1;
-        v[2] = v2;
-    }
-    inline virtual ~Triangle () {}
-    inline Triangle & operator= (const Triangle & t) {
-        v[0] = t.v[0];
-        v[1] = t.v[1];
-        v[2] = t.v[2];
-        return (*this);
-    }
-    unsigned int v[3];
+ public:
+  inline Triangle () {
+    v[0] = v[1] = v[2] = 0;
+    lable = -1;
+    tag = -1;
+  }
+  inline Triangle (const Triangle & t) {
+    v[0] = t.v[0];
+    v[1] = t.v[1];
+    v[2] = t.v[2];
+    lable = t.lable;
+    tag = t.tag;
+  }
+  inline Triangle (unsigned int v0, unsigned int v1, unsigned int v2) {
+    v[0] = v0;
+    v[1] = v1;
+    v[2] = v2;
+    lable = -1;
+    tag = -1;
+  }
+  inline virtual ~Triangle () {}
+  inline Triangle & operator= (const Triangle & t) {
+    v[0] = t.v[0];
+    v[1] = t.v[1];
+    v[2] = t.v[2];
+    lable = t.lable;
+    tag = t.tag;
+    return (*this);
+  }
+  unsigned int v[3];
+  Neighbours neighbour;
+  int lable;
+  int tag;
+};
+
+class Proxy {
+ public:
+  inline Proxy () {}
+  inline Proxy (const Vec3f & x, const Vec3f & n) : x (x), n (n) {}
+  inline virtual ~Proxy () {}
+  Vec3f x;
+  Vec3f n;
+  // TODO
+  std::vector<Triangle> T;
 };
 
 /// A Mesh class, storing a list of vertices and a list of triangles indexed over it.
 class Mesh {
-public:
-	std::vector<Vertex> V;
-	std::vector<Triangle> T;
+ public:
+  std::vector<Vertex> V;
+  std::vector<Triangle> T;
 
-    /// Loads the mesh from a <file>.off
-	void loadOFF (const std::string & filename);
+  /// Loads the mesh from a <file>.off
+  void loadOFF (const std::string & filename);
     
-    /// Compute smooth per-vertex normals
-    void recomputeNormals ();
+  /// Compute smooth per-vertex normals
+  void recomputeNormals ();
 
-    /// scale to the unit cube and center at original
-    void centerAndScaleToUnit ();
+  /// scale to the unit cube and center at original
+  void centerAndScaleToUnit ();
+
+  /// distortion error
+  float distortionError(Proxy p, Vec3f t0, Vec3f t1, Vec3f t2);
+    
+  /// shape approximation
+  void shapeApproximation() ;
+
+  /// shape approximation
+  bool isNeighbour(Triangle &T0, Triangle &T1);
+
+  void findNeighbours();
+
 };
